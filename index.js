324cb82365baf8dc71579eeac77f476844700368
Danila Makulov <attila415422@gmail.com>
2025-10-26 11:04:58 +0100

    scraping watchlists from web

--- index.js
+++ index.js
@@ -1,33 +1,56 @@
-import * as fs from 'fs';
+import readline from 'readline';
+import HTMLParser from 'node-html-parser';
 
-const csv1 = readCsv('1.csv');
-const csv2 = readCsv('2.csv');
+main();
 
+async function main() {
+  const users = await ask('Enter users: ');
+  const [username1, username2] = users.split(',').map(u => u.trim());
+  console.log({ username1, username2 });
 
-console.log(csv1[1])
-console.log(csv2[1])
+  const wl1 = await getWatchlist(username1);
+  const wl2 = await getWatchlist(username2);
 
-const common = csv1.filter(c1 => csv2.some(c2 => c2.Name === c1.Name));
-console.table(common)
+  const common = wl1.filter(f1 => wl2.some(f2 => f2.id === f1.id));
+  console.log('common:');
+  console.table(common);
+}
 
-function readCsv(filename) {
-  let result = [];
-  const data = fs.readFileSync(filename).toLocaleString();
+async function getWatchlist(username) {
+  const filmElements = [];
 
-  const rows = data.split("\n"); // SPLIT ROWS
-  rows.forEach((row) => {
-    const columns = row.split(","); //SPLIT COLUMNS
-    result.push(columns);
-  })
+  let cnt = 1;
+  while (true) {
+    const res = await fetch(`https://letterboxd.com/${username}/watchlist/page/${cnt}/`);
+    const html = await res.text();
 
-  let colNames = result[0];
-  let csv = result.slice(1);
-  csv = csv.map(row => ({
-    // [colNames[0]]: row[0],
-    [colNames[1]]: row[1],
-    [colNames[2]]: row[2],
-    [colNames[3]]: row[3],
+    const root = HTMLParser.parse(html);
+    const _filmElements = root.querySelectorAll('[data-film-id]');
+    filmElements.push(..._filmElements);
+
+    console.log(_filmElements.length);
+    if (_filmElements.length === 0) {
+      break;
+    }
+  }
+
+  const watchlist = filmElements.map(el => ({
+    name: el._attrs['data-item-name'],
+    link: el._attrs['data-item-link'],
+    id: el._attrs['data-film-id']
   }))
 
-  return csv.reverse();
+  return watchlist;
+}
+
+function ask(question) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  return new Promise(resolve => rl.question(question, answer => {
+    rl.close();
+    resolve(answer);
+  }));
 }
